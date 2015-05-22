f58c3f142b9811292e2dc60784b94e0ec2d4a6d4
CS244 QJump VM <czlee@stanford.edu>
2015-05-22 22:41:08 +0000

    Get qjump environment not to modify existing environment variables

--- qjumpm.py
+++ qjumpm.py
@@ -11,15 +11,19 @@ class QJumpManager(object):
         subprocess.call("insmod sch_qjump.ko verbose=4 timeq=15 bytesq=1550 p7rate=300 p6rate=0 p5rate=0 p4rate=150 p3rate=30 p2rate=15 p1rate=5 p0rate=1", shell=True)
 
     def create_env(self, verbosity=0, priority=0, window=9999999):
-        new_env = os.environ
+        """Creates an environment variables dict that can be passed to a
+        subprocess.Popen constructor in order to run that command with
+        qjump."""
+        new_env = dict(os.environ)
         new_env["QJAU_VERBOSITY"] = str(verbosity)
         new_env["QJAU_PRIORITY"]  = str(priority)
-        new_env["QJAU_WINDOW"]  = str(window)
-        new_env["LD_PRELOAD"]    = "./qjump-app-util.so"
+        new_env["QJAU_WINDOW"] = str(window)
+        new_env["LD_PRELOAD"] = "./qjump-app-util.so"
         return new_env
         
                
     def _install_qjump(self, node, ifname):
+        """Installs qjump on a particular node and interface."""
         _, err, exitcode = node.pexec("tc qdisc add dev %s parent 5:1 handle 6: qjump" % ifname)
         if exitcode != 0:
             print("Error binding qjump to port %s:" % ifname)
@@ -27,6 +31,7 @@ class QJumpManager(object):
         return exitcode
 
     def install_qjump(self, net):
+        """Installs qjump on all interfaces and nodes in a network."""
         results = []
         ifnames = []
         for nodename in net:
